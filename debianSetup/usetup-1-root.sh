45897166f39017ea5afd2e3203a4b1790438a169
Dan Goodman <dan@danthegoodman.com>
2018-07-08 11:45:11 -0400

    works, need to add curl now

--- debianSetup/usetup-1-root.sh
+++ debianSetup/usetup-1-root.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 echo "Who is the user that is going to be given sudo?"
 read userName
-adduser --disable-password --gecos "" $userName # Lets disable the password, and use gecos to make no prompt about user info
-adduser $userName sudo # add them to sudo group
-sudo chage -d 0 foo # Then force their password to be bad, requiring them to make one on login
+# adduser --gecos "" $userName # Lets disable the password, and use gecos to make no prompt about user info
+# adduser $userName sudo # add them to sudo group
+# useradd $userName -d /home/$userName -g sudo -m
+useradd $userName -d /home/$userName -g sudo -m -s /bin/bash; echo -e "toor\ntoor" | passwd $userName
+# echo -e "toor\ntoor" | passwd $userName
+passwd -e $userName # Then force their passwored to be expired, requiring them to make one on login
 userDir="/home/$userName"
 
-echo "#######################################"
-echo "Running apt update"
-echo "#######################################"
-apt update -y
-# sudo apt upgrade -y
+# echo "#######################################"
+# echo "Running apt update"
+# echo "#######################################"
+# apt update -y
 
 echo "#######################################"
 echo "Generating 4096 RSA ssh keys"
 echo "#######################################"
 mkdir $userDir/.ssh/
-# ssh-keygen -t RSA -b 4096 -f $userDir/.ssh/id_rsa -N ''
 grep -q "^[^#]*PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config || echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 service ssh restart
 curl -L keys.danthegoodman.com > $userDir/.ssh/authorized_keys
+# curl [LINK TO SECOND .sh FILE] > $ $userDir/2.sh
 
-curl -fsSL get.docker.com | bash
+# curl -fsSL get.docker.com | bash
 
 echo "\n\n Done... Logout and log back in as $userName. You should be prompted to create a sudo password on login."
